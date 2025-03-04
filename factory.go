a685f99c5a1433384ca1f3d8c7caa2f09dd602af
Sergey Rukin <sergey.rukin1425@gmail.com>
2025-03-04 22:54:05 +0500

    template metrics exporter

--- factory.go
+++ factory.go
@@ -14,6 +14,7 @@ import (
 func NewFactory() exporter.Factory {
 	return exporter.NewFactory(metadata.Type,
 		createDefaultConfig,
+		exporter.WithMetrics(createMetricsExporter, metadata.MetricsStability),
 		exporter.WithLogs(createLogsExporter, metadata.LogsStability),
 		exporter.WithTraces(createTracesExporter, metadata.TracesStability),
 	)
@@ -35,6 +36,26 @@ func createDefaultConfig() component.Config {
 		TimeoutSettings: exporterhelper.NewDefaultTimeoutConfig(),
 		QueueSettings: exporterhelper.NewDefaultQueueConfig(),
 	}
+}
+
+func createMetricsExporter(ctx context.Context, set exporter.Settings, cfg component.Config) (exporter.Metrics, error) {
+	config := cfg.(*Config)
+
+	exporter, err := newMetricsExporter(config, set)
+	if err != nil {
+		return nil, fmt.Errorf("failed configuring postgres metrics exporter: %w", err)
+	}
+
+	return exporterhelper.NewMetrics(
+		ctx,
+		set,
+		cfg,
+		exporter.ConsumeMetrics,
+		exporterhelper.WithStart(exporter.Start),
+		exporterhelper.WithShutdown(exporter.Shutdown),
+		exporterhelper.WithQueue(config.QueueSettings),
+		exporterhelper.WithTimeout(config.TimeoutSettings),
+	)
 }
 
 func createLogsExporter(
